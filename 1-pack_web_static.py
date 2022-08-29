821bf0b61caca18c6783426d6e101c83f881cc63
hellerdejanuar <hellerdejanuar@gmail.com>
2022-08-29 08:52:49 -0700

    1 style

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-""" module  """
+""" fabric deploy module  """
 from datetime import datetime
 from fabric.operations import local
+
 
 def do_pack():
     """ Compress a directory """
 
     local("mkdir -p versions")
-    tarball_path = 'versions/web_static_' + datetime.now().strftime("%Y%m%d%H%M%S") + '.tgz'
-    tarball = local('tar -czvf {} web_static'.format(tarball_path), capture=True)
-
-    return tarball if not tarball.failed else None
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
+    tar_path = 'versions/web_static_' + now + '.tgz'
+    tarball = local('tar -czvf {} web_static'.format(tar_path), capture=True)
+    if tarball.failed:
+        return None
+    else:
+        return tarball

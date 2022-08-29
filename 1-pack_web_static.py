276292eb5429dd41b7b0d161dff0b9576d909daa
hellerdejanuar <hellerdejanuar@gmail.com>
2022-08-29 08:49:48 -0700

    1 fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,11 +1,13 @@
-#!/usr/bin/env bash
-""" generates a .tgz archive from the contents of the web_static folder """
+#!/usr/bin/python3
+""" module  """
 from datetime import datetime
 from fabric.operations import local
 
 def do_pack():
-    local('mkdir -p versions')
-    tarball_path = 'versions/web_static_' + datetime.now().strftime(%Y%m%d%H%M%S) + '.tgz'
+    """ Compress a directory """
+
+    local("mkdir -p versions")
+    tarball_path = 'versions/web_static_' + datetime.now().strftime("%Y%m%d%H%M%S") + '.tgz'
     tarball = local('tar -czvf {} web_static'.format(tarball_path), capture=True)
 
     return tarball if not tarball.failed else None

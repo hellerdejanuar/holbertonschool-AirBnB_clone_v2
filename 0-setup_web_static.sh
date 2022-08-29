cbad6b53c47153efd108c66e7b35868bf76c88e7
hellerdejanuar <hellerdejanuar@gmail.com>
2022-08-29 07:45:55 -0700

    0 upd

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -19,12 +19,12 @@ echo "<html>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
 # symlink
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # change ownership of folder
-chown -hR ubuntu:ubuntu /data
+sudo chown -hR ubuntu:ubuntu /data
 
 
-sed -i "/server_name _/a location \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/;\n\t}" /etc/nginx/sites-available/default
+sudo sed -i "/server_name _/a location \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/;\n\t}" /etc/nginx/sites-available/default
 
 sudo service nginx restart

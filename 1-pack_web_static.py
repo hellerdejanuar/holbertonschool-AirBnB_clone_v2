5b2b4097dc91744412e965f15047c3c0f73a9a60
hellerdejanuar <hellerdejanuar@gmail.com>
2022-08-29 08:30:48 -0700

    1 add

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 """ generates a .tgz archive from the contents of the web_static folder """
 from datetime import datetime
+from fabric.operations import local
 
 def do_pack():
-    filename = 'web_static_' + datetime.now().strftime(<%Y><%m><%d><%H><%M><%S>) + '.tgz'
-    local(f'tar -czvf {filename}')
+    local('mkdir -p versions')
+    tarball_path = 'versions/web_static_' + datetime.now().strftime(%Y%m%d%H%M%S) + '.tgz'
+    tarball = local('tar -czvf {} web_static'.format(tarball_path), capture=True)
+
+    return tarball if not tarball.failed else None

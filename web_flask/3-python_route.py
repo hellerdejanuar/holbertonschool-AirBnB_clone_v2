be434e85a4dfcb369783efdd954118ad91d8ffc5
hellerdejanuar <hellerdejanuar@gmail.com>
2022-09-04 18:28:47 -0700

    3 style corrected

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -24,7 +24,7 @@ def c(text):
     return text
 
 
-@app.route("/python/", defaults={'text':'is cool'}, strict_slashes=False)
+@app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python(text):
     """ return C custom message """
